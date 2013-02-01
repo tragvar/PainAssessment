5c08c335bfed4ca2b936f53048b0d59d59af7823
tragvar <tragvar@gmail.com>
2013-02-01 14:48:58 +0200

    1

---
+++ PADrawView.h
@@ -0,0 +1,13 @@
+//
+//  PADrawView.h
+//  PainAssessment
+//
+//  Created by Admin on 2/1/13.
+//  Copyright (c) 2013 Tragvar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PADrawView : UIView
+
+@end
